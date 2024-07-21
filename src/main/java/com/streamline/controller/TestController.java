f9812a0a7ccac53e13232579b23470a18daa1553
Sana0104 <sanatabasum693@gmail.com>
2024-07-21 22:31:12 +0530

    intial commit

---
+++ src/main/java/com/streamline/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.streamline.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> getTest() {
+		return ResponseEntity.ok("Test successful!");
+	}
+	
+
+}
